254c93e43ce05b7ba4d506fa5d577daad6b0f153
Andrii Zagoruiko <andrii.zagoruiko@oracle.com>
2023-11-26 15:38:12 +0100

    currencylayer

--- bin/build_and_run.sh
+++ bin/build_and_run.sh
@@ -8,4 +8,4 @@ fi
 docker build "--tag=10.8.0.5:5000/rates-update:${VER}" ./
 docker push "10.8.0.5:5000/rates-update:${VER}"
 export NOMAD_ADDR=http://10.8.0.1:4646
-nomad job run update-rates.nomad
+nomad job run trade-update.nomad
